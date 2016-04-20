7e7aa98b087ccd437f214a40bce04bb5dff87991
EJWang <ej_wang@outlook.com>
2016-04-21 01:45:26 +0200

    Write the base code of the Main class

---
+++ src/driver/Main.java
@@ -0,0 +1,19 @@
+package driver;
+
+/**
+ * The entry-point of the GPAAssistant.
+ *
+ * @author EJWang
+ */
+public class Main {
+
+  /**
+   * The entry-point of the GPAAssistant.
+   *
+   * @param args The array of arguments pass to this function
+   */
+  public static void main(String[] args) {
+
+    // TODO: Completed the running statement
+  }
+}
