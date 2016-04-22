627e6266da3070ed02a2d9f3cc4bf88b178a2552
EJWang <ej_wang@outlook.com>
2016-04-21 23:10:49 -0400

    Add some attributes

--- src/module/Course.java
+++ src/module/Course.java
@@ -1,22 +1,52 @@
 package module;
 
 /**
- * A course that is taken by the student.
+ * A course that is offered from school.
  *
  * @author EJWang
  */
 public class Course {
 
   private final String courseCode;
+  private String courseStatus;
+  private double earnedGrade;
 
   /**
-   * Construct a module.Course.
+   * Construct a Course.
    *
-   * @param courseCode The course code of this course
+   * @param courseInfo The array of of course information
    */
-  public Course(String courseCode) {
-    this.courseCode = courseCode;
+  public Course(String[] courseInfo) {
+    courseCode = courseInfo[0];
+    courseStatus = courseInfo[1];
+    earnedGrade = Double.parseDouble(courseInfo[2]);
   }
 
+  /**
+   * Get the course code of this course.
+   *
+   * @return The course code of this course
+   */
+  public String getCourseCode() {
+    return courseCode;
+  }
+
+  /**
+   * Return true if this course is complete.
+   *
+   * @return True if this course is complete
+   */
+  public boolean isCompleted() {
+    return courseStatus.equals("COMPLETE");
+  }
+
+  /**
+   * Get the earned grade of this course.
+   *
+   * @return The earned grade of this course
+   */
+  public double getEarnedGrade() {
+    return earnedGrade;
+  }
 
 }
