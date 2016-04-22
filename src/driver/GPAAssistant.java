0fcca86d3b2324d156c358f29b9db4ef2117c29c
EJWang <ej_wang@outlook.com>
2016-04-22 01:42:59 -0400

    Add debug method

--- src/driver/GPAAssistant.java
+++ src/driver/GPAAssistant.java
@@ -1,6 +1,13 @@
 package driver;
 
 import controller.Controller;
+import module.Course;
+import module.CoursePartWork;
+import module.DataConfigurator;
+import module.Student;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * The entry-point of the GPAAssistant.
@@ -9,13 +16,33 @@ import controller.Controller;
  */
 public class GPAAssistant {
 
+  private static void DEBUGLoadConfig() throws IOException {
+
+    DataConfigurator configurator = new DataConfigurator("info.conf");
+    Student ej = configurator.loadData();
+    System.out.println("Student Name: " + ej.getStudentName());
+    System.out.println("School: " + ej.getSchoolName());
+    System.out.println("Academic History: ");
+
+    for (Course curr : ej.getAcademicHistory()) {
+      System.out.println(
+          "    Course Code: " + curr.getCourseCode() + " Status: " + curr
+              .getStatus() + " Current Grade: " + curr.getEarnedGrade());
+      for (CoursePartWork work : curr.getPartWorks()) {
+        System.out.println(
+            "        " + work.getName() + " Grade: " + work.getGrade()
+                + " Weight: " + work.getWeight());
+      }
+    }
+  }
+
   /**
    * The entry-point of the GPAAssistant.
    *
    * @param args The array of arguments pass to this function
    */
-  public static void main(String[] args) throws Exception {
-
-    new Controller();
+  public static void main(String[] args) throws IOException {
+    DEBUGLoadConfig();
+    //new Controller();
   }
 }
