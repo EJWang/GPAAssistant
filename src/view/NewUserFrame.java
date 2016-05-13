d76582f63002f50b44932c7c8b5b383865419d6b
LeonYuAng3NT <leonzhang1996@hotmail.com>
2016-05-12 23:45:29 -0400

    add base code for additional user frames

--- src/view/NewUserFrame.java
+++ src/view/NewUserFrame.java
@@ -1,5 +1,19 @@
 package view;
 
-public class NewUserFrame {
+
+
+import controller.Controller;
+
+public class NewUserFrame extends MainFrame {
+  
+  /**
+   *  Use inheritance to construct NewUserFrame object based on MainFrame
+   * 
+   * @param controller Default controller used for mainFrame
+   */
+  public NewUserFrame(Controller controller) {
+    super(controller);
+
+  }
 
 }
--- src/view/OldUserFrame.java
+++ src/view/OldUserFrame.java
@@ -1,5 +1,17 @@
 package view;
 
-public class OldUserFrame {
+import controller.Controller;
+
+public class OldUserFrame extends MainFrame{
+  /**
+   *  Use inheritance to construct OldUserFrame object based on MainFrame
+   * 
+   * @param controller Default controller used for mainFrame
+   */
+  public OldUserFrame(Controller controller) {
+    super(controller);
+
+  }
+
 
 }
